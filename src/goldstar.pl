f256668b1790bab8bec53238d343040e6e5cb6b8
Ca-moes <andre3000pt@gmail.com>
2020-12-23 22:51:01 +0000

    ready to run

--- src/goldstar.pl
+++ src/goldstar.pl
@@ -2,215 +2,144 @@
 :- use_module(library(clpfd)).
 :- use_module(library(lists)).
 
-test:-
-  opsi_to_opss([1,1,1,4,4,4,4,4,2,1], Ops),
-  star(0, Ops, L),
-  fail.
+test(Tips):-
+    operators(1, Tips, OpsInt),
+    opsi_to_opss(OpsInt, Ops),
+    gold_star(1, Ops),
+    fail.
 
-opsi_to_opss([],[]).
-opsi_to_opss([H|T], [Sig|Temp]):-
-  numb_signal(H, Sig),
-  opsi_to_opss(T, Temp).
-
-/*
-Encontra uma das 1.04m de possiveis combinações de operadores
-*/
-operators_rest(Ops):-
-  % Definição das Variáveis e Domínios
-  length(Ops, 10),
-  domain(Ops, 1, 4),
-  % Colocação das Restrições
-  element(1, Ops, Op1i),
-  bigger(Op1i, Ops),
-  % Pesquisa da solução
-  labeling([], Ops).
-
-operators(Ops):-
-  % Definição das Variáveis e Domínios
-  length(Ops, 10),
-  domain(Ops, 1, 4),
-  % Sem Colocação das Restrições
-  % Pesquisa da solução
-  labeling([], Ops).
-
-star(Cut, Ops, L):-
-  % Definição das Variáveis e Domínios
-  L = [A, B, C, D, E, F, G, H, I, J],
-  domain(L, 0, 9),
-
-  % Colocação das Restrições
-  all_distinct(L),
-
-  nth0(0, Ops, Op0i),
-  nth0(1, Ops, Op1i),
-  nth0(2, Ops, Op2i),
-  nth0(3, Ops, Op3i),
-  nth0(4, Ops, Op4i),
-  nth0(5, Ops, Op5i),
-  nth0(6, Ops, Op6i),
-  nth0(7, Ops, Op7i),
-  nth0(8, Ops, Op8i),
-  nth0(9, Ops, Op9i),
-
-  apply_restriction(Op0i, C, A, Op7i, I, F),
-  apply_restriction(Op1i, A, D, Op4i, G, J),
-  apply_restriction(Op9i, B, C, Op2i, D, E),
-  apply_restriction(Op8i, B, F, Op5i, H, J),
-  apply_restriction(Op3i, G, E, Op6i, I, H),
-  
-  % Pesquisa da solução
-  labeling([], L),
-  print_option_opss(L, Ops),
-  ((Cut == 1, !);(Cut == 0)).
-
-apply_restriction(Op1, Var1, Var2, Op2, Var3, Var4):-
-  apply_restriction(Op1, Var1, Var2, Value),
-  apply_restriction(Op2, Var3, Var4, Value).
-apply_restriction(+, Var1, Var2, Value):-
-  Var1+Var2 #= Value.
-apply_restriction(-, Var1, Var2, Value):-
-  Var1-Var2 #= Value.
-apply_restriction(*, Var1, Var2, Value):-
-  Var1*Var2 #= Value.
-apply_restriction(/, Var1, Var2, Value):-
-  % Var1/Var2 #= Value
-  Var1 #= Value * Var2.
+% operators(+Restricted, +Tips, -Operators)
+operators(1, Tips, Ops):-
+    % Definição das Variáveis e Domínios
+    Tips >= 3,
+    OperNumb is Tips*2,
+    length(Ops, OperNumb),
+    domain(Ops, 1, 4),
+    % Colocação das Restrições
+    element(1, Ops, Op1i),
+    bigger(Op1i, Ops),
+    % Pesquisa da solução
+    labeling([], Ops).
+operators(0, Tips, Ops):-
+    % Definição das Variáveis e Domínios
+    Tips >= 3,
+    OperNumb is Tips*2,
+    length(Ops, OperNumb),
+    domain(Ops, 1, 4),
+    % Sem Colocação das Restrições
+    % Pesquisa da solução
+    labeling([], Ops).
 
 bigger(_, []).
 bigger(Op1, [Op | Rest]) :-
     Op1 #>= Op,
     bigger(Op1, Rest).
 
+gold_star(Cut, Operators):-
+    % Definição das Variáveis e Domínios
+    length(Operators, Length),
+    length(Result, Length),
+    Upper is Length-1,
+    domain(Result, 0, Upper),
+
+    % Colocação das Restrições
+    all_distinct(Result),
+    first_restrictions(Operators, Result),
+    remaining_restrictions(Operators, Result),
+
+    % Pesquisa da solução
+    labeling([], Result),
+    print_result(Operators, Result),
+    ((Cut == 1, !);(Cut == 0)).
+    
+
+first_restrictions(Operators, Operands):-
+    length(Operators, Length),
+    Last_1 is Length-1, 
+    Last_2 is Length-2, 
+    Last_3 is Length-3, 
+    Last_4 is Length-4, 
+    nth0(0, Operands, A),
+    nth0(1, Operands, B),
+    nth0(2, Operands, C),
+    nth0(3, Operands, D),
+    nth0(4, Operands, E),
+    nth0(Last_1, Operands, LetterL1),
+    nth0(Last_2, Operands, LetterL2),
+    nth0(Last_3, Operands, LetterL3),
+    nth0(Last_4, Operands, LetterL4),
+    nth0(0, Operators, Op0i),
+    nth0(1, Operators, Op1i),
+    nth0(2, Operators, Op2i),
+    nth0(4, Operators, Op4i),
+    nth0(Last_1, Operators, OpL1i),
+    nth0(Last_3, Operators, OpL3i),
+    %% Restrições comuns a todos
+    %%% Primeira: A (Op1) B = D (Op4) E
+    apply_restriction(Op1i, A, B, Op4i, D, E),
+    %%% Segunda: L4 (OpL3) L3 = L1 (Op0) A
+    apply_restriction(OpL3i, LetterL4, LetterL3, Op0i, LetterL1, A),
+    %%% Terceira: L2 (OpL1) L1 = B (Op2) C
+    apply_restriction(OpL1i, LetterL2, LetterL1, Op2i, B, C).
+
+remaining_restrictions(Operators, Operands):-
+    [_,_|ListRest] = Operands,
+    apply_remaining_restrictions(Operators, ListRest, 6, 3).
+
+% Para prevenir adicionar restrições com 3 pontas ou menos
+apply_remaining_restrictions(Operators, _, _, _):-
+    length(Operators, Len),
+    Len =< 6.
+% No caso de adicionar a última restrição, não chama recursivamente. Verifica se o indice 1 dos operadores é igual ao penultimo indice
+apply_remaining_restrictions(Operators, Operands, Index1, Index2):-
+    length(Operators, OpsL),
+    Test is OpsL - 2,
+    Index1 == Test,
+    apply_rem_rest_helper(Operators, Operands, Index1, Index2).
+% aplica a restrição, limita a lista dos operandos, busca os próximos indices e continua a recursividade
+apply_remaining_restrictions(Operators, Operands, Index1, Index2):-
+    apply_rem_rest_helper(Operators, Operands, Index1, Index2),    
+    [_,_|OperandsRest] = Operands,
+    NewIndex1 is Index1+2,
+    NewIndex2 is Index2+2,
+    apply_remaining_restrictions(Operators, OperandsRest, NewIndex1, NewIndex2).
+
+% predicado para buscar os valores e aplicar a restrição intermédia
+apply_rem_rest_helper(Operators, Operands, Index1, Index2):-
+    nth0(0, Operands, D),
+    nth0(1, Operands, C),
+    nth0(3, Operands, B),
+    nth0(4, Operands, A),
+    nth0(Index1, Operators, Op1),
+    nth0(Index2, Operators, Op2),
+    apply_restriction(Op1, A, B, Op2, C, D).
+
+% predicado para aplicar a restrição da equação
+apply_restriction(Op1, Var1, Var2, Op2, Var3, Var4):-
+    apply_restriction(Op1, Var1, Var2, Value),
+    apply_restriction(Op2, Var3, Var4, Value).
+apply_restriction(+, Var1, Var2, Value):-
+    Var1+Var2 #= Value.
+apply_restriction(-, Var1, Var2, Value):-
+    Var1-Var2 #= Value.
+apply_restriction(*, Var1, Var2, Value):-
+    Var1*Var2 #= Value.
+% no caso da divisão aplica uma restrição extra: os valores da divisão têm de ser inteiros
+apply_restriction(/, Var1, Var2, Value):-
+    % Var1/Var2 #= Value
+    Var1 #= Value*Var2.
+
+print_result(Operators, Operands):-
+    write(Operators),write(Operands), nl.
+
+% tradução de inteiros para sinais
 numb_signal(1,+).
 numb_signal(2,-).
 numb_signal(3,*).
 numb_signal(4,/).
 
-print_option_opss(NumberList, OperatorList):-
-  nth0(0, OperatorList, Op0),
-  nth0(1, OperatorList, Op1),
-  nth0(2, OperatorList, Op2),
-  nth0(3, OperatorList, Op3),
-  nth0(4, OperatorList, Op4),
-  nth0(5, OperatorList, Op5),
-  nth0(6, OperatorList, Op6),
-  nth0(7, OperatorList, Op7),
-  nth0(8, OperatorList, Op8),
-  nth0(9, OperatorList, Op9),
-  format('[~w,~w,~w,~w,~w,~w,~w,~w,~w,~w]', [Op0, Op1, Op2, Op3, Op4, Op5, Op6, Op7, Op8, Op9]),
-  write(NumberList), nl.
-% Não está a ser usado, em vez de ser passada lista de ops é passada lista de inteiros a representar ops
-print_option_opsi(NumberList, OperatorList):-
-  nth0(0, OperatorList, Op0i), numb_signal(Op0i, Op0),
-  nth0(1, OperatorList, Op1i), numb_signal(Op1i, Op1),
-  nth0(2, OperatorList, Op2i), numb_signal(Op2i, Op2),
-  nth0(3, OperatorList, Op3i), numb_signal(Op3i, Op3),
-  nth0(4, OperatorList, Op4i), numb_signal(Op4i, Op4),
-  nth0(5, OperatorList, Op5i), numb_signal(Op5i, Op5),
-  nth0(6, OperatorList, Op6i), numb_signal(Op6i, Op6),
-  nth0(7, OperatorList, Op7i), numb_signal(Op7i, Op7),
-  nth0(8, OperatorList, Op8i), numb_signal(Op8i, Op8),
-  nth0(9, OperatorList, Op9i), numb_signal(Op9i, Op9),
-  format('[~w,~w,~w,~w,~w,~w,~w,~w,~w,~w]', [Op0, Op1, Op2, Op3, Op4, Op5, Op6, Op7, Op8, Op9]),
-  write(NumberList), nl.
-
-print_star(NumberList, OperatorList):-
-  nth0(0, NumberList, A),
-  nth0(1, NumberList, B),
-  nth0(2, NumberList, C),
-  nth0(3, NumberList, D),
-  nth0(4, NumberList, E),
-  nth0(5, NumberList, F),
-  nth0(6, NumberList, G),
-  nth0(7, NumberList, H),
-  nth0(8, NumberList, I),
-  nth0(9, NumberList, J),
-  nth0(0, OperatorList, Op0),
-  nth0(1, OperatorList, Op1),
-  nth0(2, OperatorList, Op2),
-  nth0(3, OperatorList, Op3),
-  nth0(4, OperatorList, Op4),
-  nth0(5, OperatorList, Op5),
-  nth0(6, OperatorList, Op6),
-  nth0(7, OperatorList, Op7),
-  nth0(8, OperatorList, Op8),
-  nth0(9, OperatorList, Op9),
-  write('                                                 '), nl, 
-  format('                   ~d                           ', [A]), nl,
-  write('                                                 '), nl,
-  format('                ~w     ~w                       ', [Op0, Op1]), nl,
-  write('                                                 '), nl, 
-  write('                                                 '), nl,
-  format('~d      ~w      ~d    =    ~d      ~w       ~d  ', [B,Op9,C,D,Op2,E]), nl,
-  write('                                                 '), nl, 
-  format('    ~w       =              =       ~w          ', [Op8, Op3]), nl,
-  write('                                                 '), nl, 
-  format('         ~d                   ~d                ', [F,G]), nl,
-  write('             =          =                        '), nl, 
-  write('                                                 '), nl, 
-  format('      ~w            ~d            ~w            ', [Op7,H,Op4]), nl,
-  write('                                                 '), nl, 
-  format('           ~w              ~w                   ', [Op6, Op5]), nl,
-  write('                                                 '), nl, 
-  format('   ~d                               ~d          ', [I,J]), nl,
-  write('                                                 ').
-
-/*
-                                          
-                   6                      
-                                          
-                -     x                   
-                                          
-                                          
-8      +      7    =    3      x       5  
-                                          
-    x       =              =       -      
-                                          
-         1                   9            
-             =          =                 
-                                          
-      +            4            x         
-                                          
-           +              x               
-                                          
-   0                               2      
-                                          
-
-*/
-
-/*
-
-10 posições possiveis para sinais
-4 sinais : + - * /
-10^4 = 1.048.576 Combinações possiveis de estrelas.
-
-
-numb_signal(Op1i, Op1),
-  apply_restriction(Op1, C, A, Value1),
-  numb_signal(Op2i, Op2),
-  apply_restriction(Op2, A, D, Value2),  
-  numb_signal(Op3i, Op3),
-  apply_restriction(Op3, B, C, Value3), 
-  numb_signal(Op4i, Op4),
-  apply_restriction(Op4, D, E, Value3), 
-  numb_signal(Op5i, Op5),
-  apply_restriction(Op5, B, F, Value4), 
-  numb_signal(Op6i, Op6),
-  apply_restriction(Op6, G, E, Value5), 
-  numb_signal(Op7i, Op7),
-  apply_restriction(Op7, I, F, Value1), 
-  numb_signal(Op8i, Op8),
-  apply_restriction(Op8, G, J, Value2), 
-  numb_signal(Op9i, Op9),
-  apply_restriction(Op9, I, H, Value5), 
-  numb_signal(Op10i, Op10),
-  apply_restriction(Op10, H, J, Value4), 
-
-*/
-
-write_to_file:-
-  open('test.txt', write, S1),
-  set_output(S1),
-  write('test 1234\n'),
-  flush_output(S1),
-  close(S1).
+% transforma uma lista de inteiros para uma lista de sinais
+opsi_to_opss([],[]).
+opsi_to_opss([H|T], [Sig|Temp]):-
+  numb_signal(H, Sig),
+  opsi_to_opss(T, Temp).
--- src/results.pl
+++ src/results.pl
@@ -3,78 +3,51 @@ Necessário mudar working directory para pasta de projeto no SicStus
 prolog:set_current_directory('C:/Data/Andre/Work/MIEIC_A3S1/PLOG/feup-plog-proj-2'), consult('C:/Data/Andre/Work/MIEIC_A3S1/PLOG/feup-plog-proj-2/src/goldstar.pl').
 */
 save_all:-
-  % mudar working directory para "Resultados_save_all" antes de executar
-  save(print_restricted_one_sol),
-  save(print_restricted_all_sol),
-  save(print_unrestricted_one_sol),
-  save(print_unrestricted_all_sol),
-  save(print_all_comb),
-  write('\nAll Finished\n').
+    save_group(3),
+    save_group(4),
+    save_group(5),
+    write('\nAll Finished\n').
 
-/*
-save(operators)
-save(print_all_comb)
-save(save_1_solution)
-*/
-save(Predicate):-
-  get_file_name(Predicate, FileName),
-  format('Saving to ~w\n', [FileName]),
-  open(FileName, write, S1),
-  current_output(Console),
-  set_output(S1),
-  statistics(runtime, [T0|_]),
-  (Predicate ; true),
-  statistics(runtime, [T1|_]),
-  T is T1 - T0,
-  format('~w took ~3d sec.~n', [Predicate, T]),
-  close(S1),
-  set_output(Console),
-  format('~w took ~3d sec.~n', [Predicate, T]).
-  
-get_file_name(Predicate, FileName):-
-  Predicate =.. L,
-  nth0(0, L, Name),
-  atom_concat(Name, '.txt', FileName).
-  
-  
-% Usado sem cut em star, encontra todas as soluções para todas as disposições
-% calcula operadores dentro de star sem usar restrições mas sim undos
-print_all_comb:-
-  star(0,_,_), fail.
+save_group(Tips):-
+    save(Tips, 1, 1),
+    save(Tips, 1, 0),
+    save(Tips, 0, 1),
+    save(Tips, 0, 0).
 
-/*
-Com Cut em Star:
-  - Arranja uma solução para cada disposição com restrições
-*/
-print_restricted_one_sol:-
-  operators_rest(Opsi),
-  opsi_to_opss(Opsi, Ops),
-  star(1, Ops, _), fail.
+save(Tips, Rest, Cut):-
+    Predicate =.. [run, Tips, Rest, Cut],
+    file_name(Tips, Rest, Cut, FileName),
+    format('Saving to ~w\n', [FileName]),
+    open(FileName, write, S1),
+    current_output(Console),
+    set_output(S1),
+    statistics(runtime, [T0|_]),
+    (Predicate ; true),
+    statistics(runtime, [T1|_]),
+    T is T1 - T0,
+    format('~w took ~3d sec.~n', [Predicate, T]),
+    close(S1),
+    set_output(Console),
+    format('~w took ~3d sec.~n', [Predicate, T]).
 
-/*
-Sem Cut em Star:
-  - Aplica restrições ás combinações de operadores e imprime todas as soluções 
-  para todas as disposições
-*/
-print_restricted_all_sol:-
-  operators_rest(Opsi),
-  opsi_to_opss(Opsi, Ops),
-  star(0, Ops, _), fail.
+run(Tips, Restricted, Cut):-
+    operators(Restricted, Tips, OpsInt),
+    opsi_to_opss(OpsInt, Ops),
+    gold_star(Cut, Ops),
+    fail.
 
-/*
-Com Cut em Star:
-  - Arranja uma solução para cada disposição sem restrições, 1.04m de resultados
-*/
-print_unrestricted_one_sol:-
-  operators(Opsi),
-  opsi_to_opss(Opsi, Ops),
-  star(1, Ops, _), fail.
-
-/*
-Sem Cut em Star:
-  - Arranja todas as soluções para todas as disposições sem restrições
-*/
-print_unrestricted_all_sol:-
-  operators(Opsi),
-  opsi_to_opss(Opsi, Ops),
-  star(0, Ops, _), fail.
+% file_name(+Tips, +Restricted, +Cut, -Filename)
+file_name(Tips, Restricted, Cut, FileName):-
+    number_chars(Tips, Y),
+    atom_chars(TipsString, Y),
+    restricted_string(Restricted, ResString),
+    cut_string(Cut, CutString),
+    atom_concat(TipsString, '_tips_', Temp),
+    atom_concat(Temp, ResString, Temp2),
+    atom_concat(Temp2, CutString, Temp3),
+    atom_concat(Temp3, '.txt', FileName).
+    
+restricted_string(1, 'restricted_').
+restricted_string(0, 'unrestricted_').
+cut_string(1, 'one_solution').
+cut_string(0, 'all_solutions').
